aceed02cd82c18eb3110f4df58b9791ce75e6ce6
NiranjanStack <aniranja@uncc.edu>
2018-02-05 10:42:46 -0500

    Data pushed to mongodb and added logger

--- parse.py
+++ parse.py
@@ -9,45 +9,83 @@ import os
 from lxml import html
 import numpy as np
 import datetime
+from pymongo import MongoClient
+import json
+import logging
 
-def parse_html(html_file, path):
+dt = datetime.datetime.now()
+log_path = r'E:\python_projects\carwale\logs'
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    filename= os.path.join(log_path, 'parse_{}.log'.format(dt.date())),
+    level=logging.INFO)
+
+client = MongoClient()
+db = client.db1
+collection = db.Carwale
+
+def parse_html(html_location, path):
     """
 
-    :param html_file: html page of car brand
+    :param html_location: html page of car brand
     :param path: location to save csv
     :return: formatted data in csv
     """
 
-    dt = datetime.datetime.now()
-    with open(os.path.join(location, html_file), 'r') as file:
-        content = file.read()
+    for hfile in os.listdir(html_location):
 
-    tree = html.fromstring(content)
+        with open(os.path.join(location, hfile), 'r') as f:
+            content = f.read()
 
-    names = tree.xpath("//ul[@id='divModels']//strong/text()")
+        tree = html.fromstring(content)
 
-    price_xp = tree.xpath("//ul[@id='divModels']//div[@class='font20 margin-top15']/text()")
+        names = tree.xpath("//ul[@id='divModels']//strong/text()")
 
-    price_unit = [p[40:-14] for p in price_xp]
+        price_xp = tree.xpath("//ul[@id='divModels']//div[@class='font20 margin-top15']/text()")
 
-    price_list = [p.split(' ')[0] for p in price_unit]
-    unit_list = [p.split(' ')[1] for p in price_unit]
+        price_unit = [p[40:-14] for p in price_xp]
 
-    df = pd.DataFrame(np.column_stack([names, price_list, unit_list]),
-                      columns=['name', 'price', 'unit'])
-    df['Timestamp'] = dt
+        price_list = [float(p.split(' ')[0]) for p in price_unit]
+        unit_list = [p.split(' ')[1] for p in price_unit]
 
-    file_name = os.path.join(path, 'car_brands_{}.csv'.format(dt.date()))
-    df.to_csv(file_name, mode='a', header=False, index=False)
+        brand = tree.xpath("//h1[@class='font30 text-black']/text()")[0].replace(' Cars','')
+
+        df = pd.DataFrame(np.column_stack([names, price_list, unit_list]),
+                          columns=['name', 'price', 'unit'])
+        df['price'] = df['price'].apply(pd.to_numeric, errors='ignore')
+        df['brand'] = brand
+        df['Timestamp'] = dt
+
+
+        file_name = os.path.join(path, 'car_brands_{}.csv'.format(dt.date()))
+        logging.info('Writing {} data to {}'.format(brand, file_name))
+        df.to_csv(file_name, mode='a', header=False, index=False)
+
+        to_mongo(df)
 
     return True
 
+def to_mongo(dataframe):
+    """
+
+    :param dataframe:
+    :return:
+    """
+    # docs = json.loads(dataframe.T.to_json(date_format='iso')).values()
+    # db.Carwale.insert(docs)
+    logging.info("Inserting to Mongo Collection: Carwale")
+    db.Carwale.insert_many(dataframe.to_dict('records'))
+
+    return True
+
+
 if __name__ == "__main__":
+
     location = r'E:\python_projects\carwale\html'
 
     save_path = r'E:\python_projects\carwale\parsed_data'
     if not os.path.exists(save_path):
         os.makedirs(save_path)
 
-    for file in os.listdir(location):
-        parse_html(file, save_path)
+    parse_html(location, save_path)
--- request.py
+++ request.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
-"""request.py: Extracts pages from carwale.com"""
+"""
+request.py: Extracts pages from carwale.com
+2/5/2018 : Added Logger
+"""
 
 __author__      = "Anand Niranjan"
 
@@ -11,7 +14,17 @@ import urllib.parse
 import os
 import time
 import random
+import logging
+import datetime
 
+save_path = r'E:\python_projects\carwale\html'
+log_path = r'E:\python_projects\carwale\logs'
+dt = datetime.datetime.now()
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    filename= os.path.join(log_path, 'request_{}.log'.format(dt.date())),
+    level=logging.INFO)
 
 def get_car_data(url):
     """
@@ -29,16 +42,16 @@ def get_car_data(url):
         new_url = urllib.parse.urljoin(url, brand)
         brand_url.append(new_url)
 
-    save_path = r'E:\python_projects\carwale\html'
     if not os.path.exists(save_path):
         os.makedirs(save_path)
 
     for b_url in brand_url:
-
-        time.sleep(random.randint(1,3))
+        time_sec = random.randint(1,3)
+        time.sleep(time_sec)
         resp = requests.get(b_url)
         file_name = os.path.join(save_path, (b_url.split('/')[-2]+'.html'))
         with open (file_name, 'w') as file:
+            logging.info("Extracting {} after {} second(s).".format(file_name, time_sec))
             file.write(str(resp.text.encode('UTF-8')))
 
 
